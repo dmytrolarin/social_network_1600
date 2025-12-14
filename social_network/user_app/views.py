37eacddb31f946024ac2a5f3fa2d31a33ab6e8cb
Dmytro Larin <dmytrolarin.development@gmail.com>
2025-12-14 16:38:57 +0100

    user save to db

--- social_network/user_app/views.py
+++ social_network/user_app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpRequest 
+from django.contrib.auth.models import User
 
-# Create your views here.
-def render_registration(request):
+
+# Create your views here. 
+def render_registration(request: HttpRequest):
+    context = {'error': ' '}
     if request.method == "POST":
-        print(request.POST)
-    return render(request= request, template_name= 'user_app/registration.html')
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+        if password == confirm_password:
+           User.objects.create_user(username= username, password= password)
+        else:
+            context['error'] = "Паролі не співпадають"
+    return render(request= request, template_name= 'user_app/registration.html', context= context)
