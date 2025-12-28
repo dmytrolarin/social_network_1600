f6e053f2774875a217a75c4fba09c98bbc594657
Dmytro Larin <dmytrolarin.development@gmail.com>
2025-12-28 16:14:45 +0100

    auth

--- social_network/user_app/urls.py
+++ social_network/user_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import render_registration
+from .views import *
 
 
 urlpatterns= [
-    path(route= "registration/", view= render_registration, name= "registration")
+    path(route= "registration/", view= render_registration, name= "registration"),
+    path(route= "login/", view= render_login, name= "login"),
+    path(route= "welcome/", view= render_welcome, name= "welcome"),
+    path(route= "logout/", view= logout_user, name= "logout")
 ]
--- social_network/user_app/views.py
+++ social_network/user_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest 
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
 
 
-# Create your views here. 
 def render_registration(request: HttpRequest):
     context = {'error': ' '}
     if request.method == "POST":
@@ -11,7 +12,52 @@ def render_registration(request: HttpRequest):
         password = request.POST.get("password")
         confirm_password = request.POST.get("confirm_password")
         if password == confirm_password:
-           User.objects.create_user(username= username, password= password)
+            try:
+                User.objects.create_user(username= username, password= password)
+                return redirect("login")
+            except IntegrityError:
+                context['error'] = "Такий користувач вже iснує"
         else:
             context['error'] = "Паролі не співпадають"
-    return render(request= request, template_name= 'user_app/registration.html', context= context)
+    return render(
+        request= request, 
+        template_name= 'user_app/registration.html', 
+        context= context
+        )
+
+
+def render_login(request: HttpRequest):
+    context = {}
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        user = authenticate(request=request, username= username, password= password)
+       
+        if user != None:
+            login(request, user)
+            return redirect("welcome")
+        else:
+            context['error'] = "Невірний логін або пароль"
+
+    return render(
+        request = request, 
+        template_name= 'user_app/login.html',
+        context=context
+        )
+
+
+def render_welcome(request: HttpRequest):
+    if request.user.is_authenticated:
+        return render(
+            request= request, 
+            template_name= "user_app/welcome.html", 
+            )
+    else:
+        return redirect("login")
+    
+    
+def logout_user(request: HttpRequest):
+    logout(request)
+    return redirect("login")
+    
