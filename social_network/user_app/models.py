2c667a94bd8fb2d6e4b50b0f8ec539fe3e208e8a
Dmytro Larin <dmytrolarin.development@gmail.com>
2026-01-25 16:14:16 +0100

    crete media fiels

--- social_network/user_app/models.py
+++ social_network/user_app/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE, related_name= "profile")
     about_me = models.TextField()
+    avatar = models.ImageField(upload_to= 'avatars/', null = True, blank= True)
     
     def __str__(self):
         return f"{self.user.username}`s Profile" 
