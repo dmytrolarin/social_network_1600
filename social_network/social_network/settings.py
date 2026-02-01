fa326f4a20ca1347c218c0d6fc16a44a27d8d2f6
Dmytro Larin <dmytrolarin.development@gmail.com>
2026-02-01 16:36:19 +0100

    Post model

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_app'
+    'user_app',
+    "post_app"
 ]
 
 MIDDLEWARE = [
