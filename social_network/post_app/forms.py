2a1c6f5af93490123482290a02af9d1dc1867051
Dmytro Larin <dmytrolarin.development@gmail.com>
2026-03-15 16:08:13 +0100

    custom validation

--- social_network/post_app/forms.py
+++ social_network/post_app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Tag
 
 
@@ -14,3 +15,29 @@ class TagForm(forms.ModelForm):
     class Meta:
         model = Tag
         fields = '__all__'
+        widgets = {
+            "name": forms.TextInput(attrs={"class": 'name'})
+        }
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        
+        if not name[0].isalpha():
+            raise ValidationError('Ім`я повинно починатися тільки з літери', params={'value': name})
+        
+        return name
+    
+    def clean(self):
+        is_active = self.cleaned_data.get('is_active')
+        icon = self.cleaned_data.get('icon')
+        description = self.cleaned_data.get('description')
+        
+        if is_active and (not icon or not description):
+            raise forms.ValidationError(
+                'Вкажіть іконку та опис, якщо тег активний'
+            )
+            
+        return self.cleaned_data  
+    
+        
+    
